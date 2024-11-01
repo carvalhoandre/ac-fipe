a6e3ca090f6a055b96ddc0bf42126a38c18474f8
carvalhoandre <andre_carvalho0@live.com>
2024-11-01 14:36:14 -0300

    feat: create base project

---
+++ src/app/components/vehicle-type-selector/vehicle-type-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-type-selector',
+  standalone: true,
+  imports: [],
+  templateUrl: './vehicle-type-selector.component.html',
+  styleUrl: './vehicle-type-selector.component.css'
+})
+export class VehicleTypeSelectorComponent {
+
+}
