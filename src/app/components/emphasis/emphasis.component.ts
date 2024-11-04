a9be8f7e873f7fc99eb5ebe723fbcb995008587c
carvalhoandre <andre_carvalho0@live.com>
2024-11-04 16:55:52 -0300

    feat: emphasis

---
+++ src/app/components/emphasis/emphasis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emphasis',
+  standalone: true,
+  imports: [],
+  templateUrl: './emphasis.component.html',
+  styleUrl: './emphasis.component.css'
+})
+export class EmphasisComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { EmphasisComponent } from '../../components/emphasis/emphasis.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, EmphasisComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
